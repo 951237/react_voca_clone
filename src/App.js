03c8dc99a975e56496980cb9aab5f578f1d6c187
951237 <951237@gmail.com>
2022-01-12 02:16:03 +0900

    Doc : Update - 글자에 색 입히기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world!  
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1
+          style={{
+            color : "red",
+            backgroudColor : "pink",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+        Welcome
+        </h1>
     </div>
   );
 }
-
 export default App;
