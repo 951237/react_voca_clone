84609fca8acc327a3f5eff53cbe4365696c8300e
951237 <951237@gmail.com>
2022-02-24 01:52:07 +0900

    Doc : Update - 리액트 보카앱 1일차

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import './App.css';
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-        <h1
-          style={{
-            color : "red",
-            backgroudColor : "pink",
-          }}
-        >
-        Welcome
-        </h1>
+      <Header /> 
     </div>
   );
 }
