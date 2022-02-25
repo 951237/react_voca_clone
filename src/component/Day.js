faf1e7e3180f3e67f4f8c933862829f164998f35
951237 <951237@gmail.com>
2022-02-26 02:36:58 +0900

    Doc : Update - 리액트 보카앱 3일차 - Day.js 생성

---
+++ src/component/Day.js
@@ -0,0 +1,20 @@
+import dummy from "../db/data.json";
+
+export default function Day() {
+	// dummy.words
+	return (
+		<>
+			<table>
+				<tbody>
+					{dummy.words.map(word => (
+					<tr>
+						<td>{word.eng}</td>
+						<td>{word.kor}</td>
+					</tr>
+					))}
+				</tbody>
+			</table>
+		</>
+	)
+	
+}
