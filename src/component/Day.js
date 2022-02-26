be940d1fdac8a3b6733a2968cd980aff76a46d46
951237 <951237@gmail.com>
2022-02-26 10:06:56 +0900

    Doc : Update - 리액트 보카앱 4일차 - 워드리스트를 일차에 맞게 출력 / filter

--- src/component/Day.js
+++ src/component/Day.js
@@ -2,12 +2,15 @@ import dummy from "../db/data.json";
 
 export default function Day() {
 	// dummy.words
+	const day = 3;
+	const wordList = dummy.words.filter(word => (word.day === day));
+	console.log(wordList);
 	return (
 		<>
 			<table>
 				<tbody>
-					{dummy.words.map(word => (
-					<tr>
+					{wordList.map(word => (
+					<tr key={word.id}>
 						<td>{word.eng}</td>
 						<td>{word.kor}</td>
 					</tr>
